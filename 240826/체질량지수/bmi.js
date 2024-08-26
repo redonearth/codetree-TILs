f3059a4f84eb770af1d3f4cf419526a16b851e77
Hong Jisang <wona23@gmail.com>
2024-08-26 13:52:21 +0900

    Title: 체질량지수, Time: 181ms, Memory: 14MB, Status: Wrong Answer - CodeTree

--- 240826/체질량지수/bmi.js
+++ 240826/체질량지수/bmi.js
@@ -2,7 +2,8 @@ const fs = require('fs')
 
 const [h, w] = fs.readFileSync(0).toString().trim().split(' ').map(Number)
 
-const bmi = (h, w) => parseInt(((10000 * w) / (h * h)).toFixed(1))
+// const bmi = (h, w) => parseInt(((10000 * w) / (h * h)).toFixed(1))
+const bmi = (h, w) => parseInt((w / ((h / 100) ** 2)).toFixed(1))
 const b = bmi(h, w)
 
 console.log(b)
