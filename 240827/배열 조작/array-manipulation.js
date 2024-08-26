5ad804b1eee3be63da19cc1044980997b156ea90
Hong Jisang <wona23@gmail.com>
2024-08-27 08:54:17 +0900

    Title: 배열 조작, Time: 186ms, Memory: 14MB, Status: Wrong Answer - CodeTree

---
+++ 240827/배열 조작/array-manipulation.js
@@ -0,0 +1,18 @@
+const fs = require('fs')
+
+const input = fs.readFileSync(0).toString().trim().split('\n')
+
+const commands = [...input[0]]
+const arr = input[2].split(' ')
+
+for (let command of commands) {
+    if (command === 'R') {
+        arr.reverse()
+    }
+
+    if (command === 'D') {
+        if (arr.length) arr.shift()
+    }
+}
+
+console.log(arr.join(' '))
