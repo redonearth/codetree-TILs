d174bcef6850715b0f1002e92e0093b34bb293b0
Hong Jisang <wona23@gmail.com>
2024-08-26 14:52:35 +0900

    Title: 4번의 크기 비교, Time: 191ms, Memory: 14MB, Status: Passed - CodeTree

---
+++ 240826/4번의 크기 비교/4-time-comparison.js
@@ -0,0 +1,11 @@
+const fs = require('fs')
+
+const input = fs.readFileSync(0).toString().trim().split('\n')
+
+const a = Number(input[0])
+const [b, c, d, e] = input[1].split(' ').map(Number)
+
+console.log(a > b ? 1 : 0)
+console.log(a > c ? 1 : 0)
+console.log(a > d ? 1 : 0)
+console.log(a > e ? 1 : 0)
