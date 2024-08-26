a78b632a68bfe0bfc920e461fe84142636d03a85
Hong Jisang <wona23@gmail.com>
2024-08-26 15:50:48 +0900

    Title: 장학금, Time: 191ms, Memory: 14MB, Status: Passed - CodeTree

--- 240826/장학금/scholarship.js
+++ 240826/장학금/scholarship.js
@@ -5,7 +5,7 @@ const [mid, final] = fs.readFileSync(0).toString().trim().split(' ').map(Number)
 if (mid < 90 || final < 90) {
     console.log(0)
 } else if (final >= 95) {
-    console.log(10)
+    console.log(100000)
 } else if (final >= 90) {
-    console.log(5)
+    console.log(50000)
 }
