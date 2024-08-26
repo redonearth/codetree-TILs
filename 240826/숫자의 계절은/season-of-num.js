c2c579b75bf5c1a128446ae55eab215564e0c546
Hong Jisang <wona23@gmail.com>
2024-08-26 15:45:41 +0900

    Title: 숫자의 계절은, Time: 190ms, Memory: 14MB, Status: Passed - CodeTree

--- 240826/숫자의 계절은/season-of-num.js
+++ 240826/숫자의 계절은/season-of-num.js
@@ -2,12 +2,12 @@ const fs = require('fs')
 
 const m = Number(fs.readFileSync(0).toString().trim())
 
-if (m >= 3 && m <= 5) {
-    console.log('Spring')
-} else if (m >= 6 && m <= 8) {
-    console.log('Summer')
-} else if (m >= 9 && m <= 11) {
-    console.log('Fall')
-} else {
+if (m >= 12 || m <= 2) {
     console.log('Winter')
+} else if (m <= 5) {
+    console.log('Spring')
+} else if (m <= 8) {
+    console.log('Summer')
+} else {
+    console.log('Fall')
 }
