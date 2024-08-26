3f06544efe422b7a2e472ff8448927518dd62b9b
Hong Jisang <wona23@gmail.com>
2024-08-26 13:46:06 +0900

    Title: 체질량지수, Time: 192ms, Memory: 14MB, Status: Wrong Answer - CodeTree

---
+++ 240826/체질량지수/bmi.js
@@ -0,0 +1,13 @@
+const fs = require('fs')
+
+const input = fs.readFileSync(0).toString().trim().split(' ')
+const h = Number(input[0])
+const w = Number(input[1])
+
+const bmi = (h, w) => parseInt((10000 * w / h ** 2).toFixed(1))
+const b = bmi(h, w)
+
+console.log(b)
+if (b >= 25) {
+    console.log('Obesity')
+}
