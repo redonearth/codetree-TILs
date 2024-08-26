9c5b1a3f41cefa9a2751023220c4ede95aab44f8
Hong Jisang <wona23@gmail.com>
2024-08-26 16:24:57 +0900

    Title: 두 사람, Time: 173ms, Memory: 14MB, Status: Wrong Answer - CodeTree

---
+++ 240826/두 사람/two-person.js
@@ -0,0 +1,16 @@
+const fs = require('fs')
+
+const [A, B] = fs.readFileSync(0).toString().trim().split('\n')
+
+const a = A.split(' ')
+const b = B.split(' ')
+
+const aAge = Number(a[0]), aSex = a[1]
+const bAge = Number(b[0]), bSex = b[1]
+
+const adult = 19
+const man = 'M'
+
+if ((aAge >= adult && aSex === man) || (bAge >= adult && bSex === man)) {
+    console.log(1)
+}
