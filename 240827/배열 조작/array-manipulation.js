ac9eb452af29e7772a4e273dff1748def95818c8
Hong Jisang <wona23@gmail.com>
2024-08-27 09:17:31 +0900

    Title: 배열 조작, Time: 5021ms, Memory: 18MB, Status: Time Limit Exceed - CodeTree

--- 240827/배열 조작/array-manipulation.js
+++ 240827/배열 조작/array-manipulation.js
@@ -3,21 +3,13 @@ const fs = require('fs')
 const input = fs.readFileSync(0).toString().trim().split('\n')
 
 const commands = [...input[0]]
-const n = Number(input[1])
 const arr = input[2].split(' ')
 
-for (let i = 0; i <= n; i++) {
-    const command = commands[i]
-
-    if (command === 'D') {
-        if (!arr.length) {
-            return console.log('error')
-        }
-        arr.shift()
-    }
-
+for (let command of commands) {
     if (command === 'R') {
         arr.reverse()
+    } else if (command === 'D' && arr.length) {
+        arr.shift()
     }
 }
 
