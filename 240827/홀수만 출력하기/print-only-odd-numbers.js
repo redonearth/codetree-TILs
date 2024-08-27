f6695abc0770259da6d54babb4be084bc7ac4cda
Hong Jisang <wona23@gmail.com>
2024-08-27 14:10:10 +0900

    Title: 홀수만 출력하기, Time: 220ms, Memory: 14MB, Status: Wrong Answer - CodeTree

--- 240827/홀수만 출력하기/print-only-odd-numbers.js
+++ 240827/홀수만 출력하기/print-only-odd-numbers.js
@@ -1,8 +1,10 @@
 const fs = require('fs')
 
-const input = fs.readFileSync(0).toString().trim().split('\n').map(Number)
+const input = fs.readFileSync(0).toString().trim().split('\n')
 
-for (let i = 0; i < input.length; i++) {
+const N = input[0]
+
+for (let i = 1; i < N; i++) {
     let n = input[i]
 
     if (n % 2 !== 0 && n % 3 === 0) {
