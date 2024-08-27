480e72e30e6553b9c3854d22387dbe30c643bfdd
Hong Jisang <wona23@gmail.com>
2024-08-27 13:55:48 +0900

    Title: 369 게임, Time: 169ms, Memory: 47MB, Status: Wrong Answer - CodeTree

--- 240827/369 게임/369-game.js
+++ 240827/369 게임/369-game.js
@@ -5,10 +5,7 @@ const n = Number(fs.readFileSync(0).toString().trim())
 const answer = []
 
 for (let i = 1; i <= n; i++) {
-    if (i % 3 === 0
-    //  || [...i.toString()].some(v => v % 3 === 0)
-     ) {
-        console.log(i)
+    if (i % 3 === 0 || [...i.toString()].some(v => Number(v) % 3 === 0)) {
         answer.push(0)
     } else {
         answer.push(i)
