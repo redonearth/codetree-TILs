bbc5adbeabc18bd26f4ff8048fda5b02f9ad58cd
Hong Jisang <wona23@gmail.com>
2024-09-02 13:41:05 +0900

    Title: a의 b승, Time: 188ms, Memory: 14MB, Status: Passed - CodeTree

--- 240902/a의 b승/a-to-the-power-of-b.js
+++ 240902/a의 b승/a-to-the-power-of-b.js
@@ -2,4 +2,10 @@ const fs = require('fs')
 
 const [a, b] = fs.readFileSync(0).toString().trim().split(' ').map(Number)
 
-console.log(a ** b)
+let prod = 1
+
+for (let i = 0; i < b; i++) {
+    prod *= a
+}
+
+console.log(prod)
