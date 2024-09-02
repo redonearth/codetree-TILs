d34b212eaf2fb2113d04dc607fe09c0b6a6026a6
Hong Jisang <wona23@gmail.com>
2024-09-02 16:41:20 +0900

    Title: 1까지 나누기, Time: 178ms, Memory: 47MB, Status: Passed - CodeTree

---
+++ 240902/1까지 나누기/divide-by-1.js
@@ -0,0 +1,14 @@
+const fs = require('fs')
+
+let n = Number(fs.readFileSync(0).toString().trim())
+
+let cnt = 0
+
+for (let i = 1; i <= 5000; i++) {
+    n = parseInt(n / i)
+    cnt++
+
+    if (n <= 1) break
+}
+
+console.log(cnt)
