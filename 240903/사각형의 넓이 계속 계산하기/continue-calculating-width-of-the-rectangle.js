bdaa12120168f4c2473081c620ddef3abb9dc0e7
Hong Jisang <wona23@gmail.com>
2024-09-03 08:32:17 +0900

    Title: 사각형의 넓이 계속 계산하기, Time: 204ms, Memory: 47MB, Status: Passed - CodeTree

--- 240903/사각형의 넓이 계속 계산하기/continue-calculating-width-of-the-rectangle.js
+++ 240903/사각형의 넓이 계속 계산하기/continue-calculating-width-of-the-rectangle.js
@@ -4,11 +4,11 @@ const input = fs.readFileSync(0).toString().trim().split('\n')
 
 let index = 0
 
-do {
+while (true) {
     let [width, height, char] = input[index].split(' ')
     index++
 
     console.log(Number(width) * Number(height))
 
     if (char === 'C') break
-} while (true)
+}
