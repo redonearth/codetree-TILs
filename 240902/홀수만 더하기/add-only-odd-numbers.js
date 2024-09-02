683efdbda2e42c3caeed80e66f7deef26f7f7f96
Hong Jisang <wona23@gmail.com>
2024-09-02 10:59:44 +0900

    Title: 홀수만 더하기, Time: 190ms, Memory: 14MB, Status: Passed - CodeTree

--- 240902/홀수만 더하기/add-only-odd-numbers.js
+++ 240902/홀수만 더하기/add-only-odd-numbers.js
@@ -4,8 +4,8 @@ const input = fs.readFileSync(0).toString().trim().split('\n').map(Number)
 
 let oddSum = 0
 
-for (let i = 0; i < input.length; i++) {
-    if (input[i] % 2 === 1 && input[i] % 3 === 0) {
+for (let i = 1; i < input.length; i++) {
+    if (input[i] % 2 !== 0 && input[i] % 3 === 0) {
         oddSum += input[i]
     }
 }
