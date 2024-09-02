5c5b625eab9184d1b74a4c3d3f1f023e5d3346af
Hong Jisang <wona23@gmail.com>
2024-09-02 10:16:34 +0900

    Title: 배수의 개수 2, Time: 188ms, Memory: 14MB, Status: Passed - CodeTree

--- 240902/배수의 개수 2/number-of-multipliers-2.js
+++ 240902/배수의 개수 2/number-of-multipliers-2.js
@@ -5,7 +5,7 @@ const input = fs.readFileSync(0).toString().trim().split('\n')
 let cnt = 0
 
 for (let i = 0; i < input.length; i++) {
-    if (i % 2 !== 0) cnt++
+    if (input[i] % 2 !== 0) cnt++
 }
 
 console.log(cnt)
