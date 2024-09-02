d7ca6ee378c2a066ae72b7dc81597a240c1ffd38
Hong Jisang <wona23@gmail.com>
2024-09-02 11:24:17 +0900

    Title: 5의 배수의 합, Time: 201ms, Memory: 15MB, Status: Passed - CodeTree

--- 240902/5의 배수의 합/sum-of-specific-condition-multiple.js
+++ 240902/5의 배수의 합/sum-of-specific-condition-multiple.js
@@ -4,7 +4,10 @@ const [a, b] = fs.readFileSync(0).toString().trim().split(' ').map(Number)
 
 let sumM5 = 0
 
-for (let i = a; i <= b; i++) {
+const min = Math.min(a, b)
+const max = Math.max(a, b)
+
+for (let i = min; i <= max; i++) {
     if (i % 5 === 0) {
         sumM5 += i
     }
