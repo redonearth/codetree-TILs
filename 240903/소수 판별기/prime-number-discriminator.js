de9835eaceaedccf789c42da8cee388e6b51682a
Hong Jisang <wona23@gmail.com>
2024-09-03 13:35:01 +0900

    Title: 소수 판별기, Time: 171ms, Memory: 47MB, Status: Passed - CodeTree

---
+++ 240903/소수 판별기/prime-number-discriminator.js
@@ -0,0 +1,13 @@
+const fs = require('fs')
+
+const n = Number(fs.readFileSync(0).toString().trim())
+
+let isPrime = true
+
+for (let i = 2; i <= Math.sqrt(n); i++) {
+    if (n % i === 0) {
+        isPrime = false
+    }
+}
+
+console.log(isPrime ? 'P' : 'C')
