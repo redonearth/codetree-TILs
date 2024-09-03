e026e65eb991e9b9ad4d6b955029cb10527d6963
Hong Jisang <wona23@gmail.com>
2024-09-03 16:08:04 +0900

    Title: 별표 출력하기 4, Time: 175ms, Memory: 47MB, Status: Passed - CodeTree

---
+++ 240903/별표 출력하기 4/print-star-4.js
@@ -0,0 +1,21 @@
+const fs = require('fs')
+
+const n = Number(fs.readFileSync(0).toString().trim())
+
+let str = ''
+
+for (let i = 0; i < n; i++) {
+    str = ''
+    for (let j = n; j > i; j--) {
+        str += '* '
+    }
+    console.log(str)
+}
+
+for (let i = 1; i < n; i++) {
+    str = ''
+    for (let j = 0; j <= i; j++) {
+        str += '* '
+    }
+    console.log(str)
+}
