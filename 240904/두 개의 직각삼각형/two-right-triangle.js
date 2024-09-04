fe0b5c49193878327f2d4eab44145fc6583109d7
Hong Jisang <wona23@gmail.com>
2024-09-04 09:18:54 +0900

    Title: 두 개의 직각삼각형, Time: 177ms, Memory: 47MB, Status: Passed - CodeTree

--- 240904/두 개의 직각삼각형/two-right-triangle.js
+++ 240904/두 개의 직각삼각형/two-right-triangle.js
@@ -6,11 +6,10 @@ let str = ''
 
 for (let i = 0; i < n; i++) {
     str = ''
-    for (let j = 1; j < n - 1; j++) {
-        for (let k = 0; k < n - i; k++) {
+    for (let j = 0; j < n * 2; j++) {
+        if (j < n - i || j >= n + i) {
             str += '*'
-        }
-        for (let k = 1; k < i * 2 + 1; k++) {
+        } else {
             str += ' '
         }
     }
