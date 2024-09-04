67c7e9b789156299b724af6f144be337909d36a2
Hong Jisang <wona23@gmail.com>
2024-09-04 16:30:19 +0900

    Title: 좌우반전시킨 정사각형, Time: 186ms, Memory: 14MB, Status: Passed - CodeTree

---
+++ 240904/좌우반전시킨 정사각형/left-right-inverted-square.js
@@ -0,0 +1,13 @@
+const fs = require('fs')
+
+const n = Number(fs.readFileSync(0).toString().trim())
+
+let str = ''
+
+for (let i = 1; i <= n; i++) {
+    str = ''
+    for (let j = n; j > 0; j--) {
+        str += `${i * j} `
+    }
+    console.log(str)
+}
